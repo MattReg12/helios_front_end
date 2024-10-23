4876dc59e29ab2f5b2b08ce026a6a36a6571f0c6
Matt Register <rmregister0510@gmail.com>
2024-10-23 16:48:21 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import LandingPage from './components/LandingPage'
+import MainPage from './components/MainPage'
+
+function App() {
+  return (
+    <>
+      <LandingPage />
+      {/* <MainPage /> */}
+    </>
+  )
+}
+
+export default App
